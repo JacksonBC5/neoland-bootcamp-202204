84c37ddd42982eb61a06ad5e026e16ddc05eaef0
jacksonbc5 <jacintblanch5@gmail.com>
2022-05-25 09:03:09 +0200

    promises

---
+++ staff/jacint-blanch/notes/README.md
@@ -0,0 +1,4 @@
+# Apertura
+
+
+# Notes
---
+++ staff/jacint-blanch/notes/src/utils/index.js
@@ -0,0 +1,9 @@
+const createCustomError = require('./createCustomError')
+const createId = require('./createId')
+const deleteFiles = require('./deleteFiles')
+
+module.exports = {
+    createCustomError,
+    createId,
+    deleteFiles
+}
